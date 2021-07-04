da2d66309a0603026d8cc3ea42a4d2a7aeca4ca6
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-04 19:55:14 +0530

    Element Deleted From LinkedList

--- Main.java
+++ Main.java
@@ -1,16 +1,18 @@
 package com.Bridgelabz;
 
-public class Main {
-		public static void main(String[] args) {
-			System.out.println("Welcome to DataStructure Program");
-			LinkedList linkedList = new LinkedList();
-			
-			linkedList.insert(56);
-	    	linkedList.insert(30);
-	    	linkedList.insert(70);
-	    	int pop = linkedList.pop();
-	    	System.out.println(pop);
-	    	linkedList.print();
-		}
-	}
+public class Main{
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to DataStructure Program");
+		LinkedList linkedList = new LinkedList();
+
+		linkedList.insert(56);
+		linkedList.insert(30);
+		linkedList.insert(70);
+		int pop = linkedList.pop();
+		System.out.println(pop);
+		linkedList.print();
+
+		System.out.println("Deleted Element From LinkedList");
+	}
+}
